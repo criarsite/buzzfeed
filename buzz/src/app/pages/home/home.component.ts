c37813fecc1ed1f36a541440d371c1dfbe1c5e50
criarsite <oktuga@gmail.com>
2023-12-02 22:49:51 +0000

    Atualizando

--- buzz/src/app/pages/home/home.component.ts
+++ buzz/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { BuzzComponent } from "../../components/buzz/buzz.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [BuzzComponent]
 })
 export class HomeComponent {
 
